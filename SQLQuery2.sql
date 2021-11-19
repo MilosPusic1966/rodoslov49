5d4bb5c7e91905b71cb3ed4d481980b27b208606
MilosPusic2021 <92999387+MilosPusic2021@users.noreply.github.com>
2021-11-19 19:05:54 +0100

    unuci - TO DO

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -20,3 +20,34 @@ SELECT * FROM Osoba WHERE ID_tata =
 (SELECT ID_tata FROM Osoba WHERE ID=8)
 AND NOT ID=8
 
+CREATE FUNCTION Roditelj (@ID INT, @pol CHAR)
+RETURNS INT
+AS
+BEGIN
+DECLARE @Rezultat INT
+  IF @pol='m' SET @Rezultat= (
+  SELECT ID_tata FROM Osoba 
+  WHERE ID=@ID)
+  ELSE
+  SET @Rezultat=
+  (SELECT ID_mama FROM Osoba 
+  WHERE ID=@ID) 
+  RETURN @Rezultat
+END
+
+SELECT * FROM Osoba WHERE ID=dbo.Roditelj(1, 'z')
+SELECT * FROM Osoba WHERE 
+ID=dbo.Roditelj(dbo.roditelj(1,'z'),'m')
+
+CREATE FUNCTION Deca(@ID INT)
+RETURNS TABLE
+AS
+RETURN SELECT ID FROM Osoba
+WHERE ID_tata = @ID OR ID_mama = @ID
+
+SELECT * FROM Osoba WHERE ID IN 
+(SELECT * FROM dbo.Deca(8))
+
+-- domaci: unuci date osobe
+SELECT * FROM dbo.Deca(dete) JOIN 
+SELECT ID as dete FROM dbo.Deca(13)
