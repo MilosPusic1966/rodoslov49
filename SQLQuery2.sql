ce8d0fb334e6d187d6ee9218fbfbcfa771d320dd
MilosPusic2021 <92999387+MilosPusic2021@users.noreply.github.com>
2021-11-19 18:08:56 +0100

    Baba

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -4,3 +4,11 @@ SELECT ime, prezime, pol
 FROM Osoba
 WHERE ID= 
 (SELECT ID_mama FROM Osoba WHERE ID=1)
+
+-- podaci o babi po tati osobe sa id=1
+SELECT ime, prezime
+FROM Osoba WHERE ID=(
+SELECT ID_mama FROM Osoba WHERE ID=
+(SELECT ID_tata FROM Osoba WHERE ID=1)
+)
+-- deca date osobe
